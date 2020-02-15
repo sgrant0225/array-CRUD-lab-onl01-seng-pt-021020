fb310e5e4598a3619a2a364a9dde1f4e249fd768
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-15 14:13:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
+require 'pry'
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = ["sad", "glad", "bad", "mad"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
